ffd67f9d5a1fd5a287cc0339cdd35162ad31260c
Sami353 <malisami878@gmail.com>
2023-02-17 09:09:41 +0545

    saving changes

--- App.js
+++ App.js
@@ -3,7 +3,22 @@ import { ThemeProvider } from "styled-components/native";
 import { RestaurantScreen } from "./src/features/restaurant/screens/restaurant.screen";
 import { theme } from "./src/infrastructure/theme/index";
 
+import { useFonts as useOswald, Oswald_400Regular } from "expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "expo-google-fonts/lato";
+console.log(StatusBar.currentHeight);
+
 export default function App() {
+  let[oswaldLoaded]=useFonts({
+    Oswald_400Regular,
+  });
+  let[latoLoaded]=useFonts({
+    Lato_400Regular,
+  });
+  
+  if(!oswaldLoaded || !latoLoaded){
+    return null;
+  
+
   return (
     <>
       <ThemeProvider theme={theme}>
@@ -19,3 +34,4 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+}
--- src/features/restaurant/components/restaurant-info-card.components.js
+++ src/features/restaurant/components/restaurant-info-card.components.js
@@ -2,29 +2,40 @@ import react from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import styled from "styled-components/native";
+import { SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
 
 const Title = styled.Text`
-padding: ${(props) => props.theme.space[3]};
-padding-left: ${(props) => props.theme.space[4]};
-color: ${(props) => props.theme.colors.ui.primary}  ;
+  padding: ${(props) => props.theme.space[3]};
+  padding-left: ${(props) => props.theme.space[4]};
+  color: ${(props) => props.theme.colors.ui.primary};
+  font-family: ${(props) => props.theme.fonts.heading};
 `;
 
 const RestaurantCard = styled(Card)`
-background-color: ${(props) => props.theme.colors.bg.primary};
-margin:${(props) => props.theme.space[4]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+  margin: ${(props) => props.theme.space[4]};
 `;
 
-const RestaurantCardCover= styled(Card.Cover)`
-padding: ${(props) => props.theme.space[4]};
-background-color:${(props) => props.theme.colors.bg.primary};
-`
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: ${(props) => props.theme.space[4]};
+  background-color: ${(props) => props.theme.colors.bg.primary};
+`;
+const Address = styled.Text`
+  font-family: ${(props) => props.theme.fonts.body};
+  font-size: ${(props) => props.theme.fontSixes.caption};
+`;
+
+const Info = styled.View`
+  padding: ${(props) => props.theme.space[4]};
+`;
 
 export const RestaurantInfo = ({ restaurant = {} }) => {
   const {
     name = "Hari dai ko Tarkari Pasal",
     icon,
     photos = [
-      "https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1", 
+      "https://images.pexels.com/photos/1267320/pexels-photo-1267320.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     ],
     address = "pul muni bato side ma",
     rating = 4,
@@ -32,9 +43,13 @@ export const RestaurantInfo = ({ restaurant = {} }) => {
     isClosedTemporaroily,
   } = restaurant;
   return (
-    <RestaurantCard elvation={5}>
+    <RestaurantCard elevation={5}>
       <RestaurantCardCover source={{ uri: photos[0] }} />
-      <Title>{name}</Title>
+      <Info>
+        <Title>{name}</Title>
+        <SvgXml sml={star} width={20} height={20}/>
+        <Address>{address}</Address>
+      </Info>
     </RestaurantCard>
   );
 };
